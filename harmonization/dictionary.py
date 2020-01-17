1a522bad64e3b3358e242b1d1d46e00c3f7810fd
Samuel St-Jean <samuel.st-jean@usherbrooke.ca>
2020-01-17 12:12:43 +0100

    further cleanup

--- harmonization/dictionary.py
+++ harmonization/dictionary.py
@@ -24,14 +24,6 @@ def get_global_D(datasets, outfilename, block_size, ncores=None, batchsize=32, n
     # get the data shape so we can preallocate some arrays
     # we also have to assume all datasets have the same 3D shape obviously
     shape = nib.load(datasets[0]['data']).header.get_data_shape()
-
-    # if len(block_size) < len(shape):
-    #     # In the event that we only give out a 3D size and that we have b0s with different TE/TR, we remove those volumes
-    #     # Therefore, we need to look at the total number of kept volumes, rather than the shape, to specify the last dimension properly
-    #     last_shape = get_indexer(datasets[0]).sum() - 1  # We subtract 1 because block_size adds one b0 to it later down
-    #     current_block_size = block_size + (last_shape,)
-    #     print('Using full 4D stuff')
-    # else:
     current_block_size = block_size
 
     n_atoms = int(np.prod(current_block_size) * 2)
@@ -46,10 +38,8 @@ def get_global_D(datasets, outfilename, block_size, ncores=None, batchsize=32, n
 
         print('Now feeding dataset {}'.format(filename['data']))
 
-        # indexer = get_indexer(filename)
         mask = nib.load(filename['mask']).get_fdata(caching='unchanged').astype(np.bool)
         data = nib.load(filename['data']).get_fdata(caching='unchanged').astype(np.float32) * mask[..., None]
-        # data = data[..., indexer]
         bvals = np.loadtxt(filename['bval'])
         bvecs = np.loadtxt(filename['bvec'])
 
@@ -120,7 +110,6 @@ def get_global_D(datasets, outfilename, block_size, ncores=None, batchsize=32, n
                     variance = extract_patches(variance, b0_block_size, overlap)
                     axis = tuple(range(variance.ndim//2, variance.ndim))
                     variance = np.median(variance, axis=axis)[mask_patch].ravel()
-                    print('variance shape', variance.shape)
                 except IOError:
                     print('Volume {} not found!'.format(filename['std']))
                     variance = [None]
@@ -130,10 +119,6 @@ def get_global_D(datasets, outfilename, block_size, ncores=None, batchsize=32, n
             # check to build with np.r_ the whole list from stringnames instead
             train_list += [patches]
             variance_large += list(variance)
-            # train_data.extend(patches)
-            # variance_large.extend(variance)
-
-        print('train', len(train_list), data.shape, b0_block_size, overlap, patches.shape)
 
         del data, mask, patches, variance
 
@@ -141,7 +126,6 @@ def get_global_D(datasets, outfilename, block_size, ncores=None, batchsize=32, n
 
     lengths = [l.shape[0] for l in train_list]
     train_data = np.empty((np.sum(lengths), np.prod(b0_block_size)))
-    print(train_data.shape)
 
     step = 0
     for i in range(len(train_list)):
@@ -151,9 +135,6 @@ def get_global_D(datasets, outfilename, block_size, ncores=None, batchsize=32, n
         step += length
 
     del train_list
-
-    # if center:
-    #     train_data -= train_data.mean(axis=1, keepdims=True)
 
     # we have variance as a N elements list - so check one element to see if it's an array
     if variance_large[0] is not None:
@@ -187,7 +168,6 @@ def rebuild(data, mask, D, block_size, block_up, ncores=-1,
                  last)
     overlap = (1, 1, 1, last)
     new_overlap = overlap
-    print(new_shape, new_overlap, factor)
 
     if block_size == block_up:
         D_depimpe = np.copy(D)
@@ -196,25 +176,13 @@ def rebuild(data, mask, D, block_size, block_up, ncores=-1,
 
     blocks = extract_patches(data, block_size, overlap).reshape(-1, np.prod(block_size))
     del data
-    # original_shape = blocks.shape
-
-    # # check if recon is correct / only reconstruct old school way from the blocks to be on the safe side maybe?
-    # recon = reconstruct_from_blocks(blocks, new_shape, block_size, block_up[:-1], new_overlap, weights=None)
-    # return recon
-
-    # blocks = np.asarray(blocks).reshape(-1, np.prod(block_size))
 
     # get the variance as blocks
     if variance is not None:
         variance *= mask
-        print(variance.shape)
         variance = extract_patches(variance, block_size[:-1], overlap[:-1])
-        print(variance.shape)
-        # axis = list(range(variance.ndim//2, variance.ndim))
-        # variance = np.median(variance, axis=axis)
-        # print(variance.shape, np.prod(block_size[:-1]), np.prod(block_size), np.prod(variance.shape))
         variance = np.asarray(variance).reshape(-1, np.prod(block_size[:-1]))
-        print(variance.shape)
+
     # skip empty rows from training since they are probably masked background
     mask = blocks.sum(axis=1) > np.prod(block_size) // 2
 
@@ -224,37 +192,15 @@ def rebuild(data, mask, D, block_size, block_up, ncores=-1,
         # if we are on an edge, variance can be 0, so truncate those cases as well
         np.logical_and(mask, variance > 0, out=mask)
         variance = variance[mask]
-        print(variance.shape, np.sum(variance == 0))
 
-    print(blocks.shape)
     blocks = blocks[mask]
-    print(blocks.shape, D.shape, D_depimpe.shape, mask.shape, block_size, block_up, new_shape, new_overlap, 'pre l1 stuff')
 
-    # if center:
-    #     blocks_mean = blocks.mean(axis=1, keepdims=True)
-    #     blocks -= blocks_mean
     tt = time()
     X_small_denoised, alpha, intercept, _ = solve_l1(blocks, D_depimpe, variance=variance, return_all=True, nlambdas=100, use_joblib=True,
                                                      positivity=positivity, fit_intercept=True, standardize=True, progressbar=True,
                                                      ncores=ncores, use_crossval=use_crossval)
 
-    print(X_small_denoised.shape, D_depimpe.shape, alpha.shape, intercept.shape)
-    # print(np.min(alpha), np.max(alpha), np.abs(alpha).min(), np.abs(alpha).max())
-    # print(np.min(intercept), np.max(intercept), np.abs(intercept).min(), np.abs(intercept).max())
     print('total time was {}'.format(time() - tt))
-    # if fix_mean:
-    #     mean = blocks.mean(axis=1)
-    # else:
-    #     mean = None
-
-    # if center:
-    #     intercept += blocks_mean
-
-    # # reconstructor = None  # should we put block_up from the original?
-    # tt = time()
-    # recon = reconstruct_from_indexes(alpha, D, intercept, new_shape, new_overlap, mask, block_size, block_up)  #
-    # print('reconstruction took {}'.format(time() - tt))
-    # return recon
 
     # we actually only want alpha and intercept in this step and throw out X if we do upsampling normally
     # for ease of use reason, we now use directly X for the reconstruction, but we should
@@ -289,7 +235,6 @@ def harmonize_my_data(dataset, kwargs):
     center = kwargs['center']
     block_up = literal_eval(kwargs['block_up'])
     block_size = literal_eval(kwargs['block_size'])
-    # use_std = kwargs['use_std']
     positivity = kwargs['positivity']
     fit_intercept = kwargs['fit_intercept']
     center = kwargs['center']
@@ -297,54 +242,25 @@ def harmonize_my_data(dataset, kwargs):
     use_crossval = kwargs['use_crossval']
     split_b0s = kwargs['split_b0s']
     b0_threshold = kwargs['b0_threshold']
-    # batchsize = kwargs['batchsize']
-    # niter = kwargs['niter']
     ncores = kwargs['ncores']
 
     print('Now rebuilding {}'.format(dataset['data']))
     D = np.load(path_D)
     predicted_D = path_D.replace('.npy', '')
+
     output_filename = dataset['data'].replace('.nii', '_predicted_' + predicted_D + '.nii.gz')
     output_filename = output_filename.replace('.nii.gz', '_recon.nii.gz')
     output_filename = os.path.join(outpath, os.path.basename(output_filename))
 
-    # if center:
-    #     output_filename = output_filename.replace('_predicted_', '_predicted_center_')
-
-    # if use_crossval:
-    #     output_filename = output_filename.replace('_predicted_', '_predicted_with_cv2_')
-    # else:
-    #     output_filename = output_filename.replace('_predicted_', '_predicted_with_aic_')
-
-    # if use_std:
-    #     output_filename = output_filename.replace('_predicted_', '_predicted_with_std_')
-
-    # if positivity:
-    #     output_filename = output_filename.replace('_predicted_', '_predicted_pos_')
-
-    # if fix_mean:
-    #     output_filename = output_filename.replace('_predicted_', '_predicted_meanfix_')
-
-    # if not fit_intercept:
-    #     output_filename = output_filename.replace('_predicted_', '_predicted_no_intercept_')
-
     if os.path.isfile(output_filename):
         print('File already exists! Skipping {}'.format(output_filename))
     else:
-    # if True:
         vol = nib.load(dataset['data'])
-
-        # once we have loaded the data, replace the name if we used a fw dataset since everything else will match
-        # dataset = dataset.replace('_fw', '')
-        # to_dataset = to_dataset.replace('_fw', '')
-
-        # indexer = get_indexer(dataset)
         data = vol.get_fdata(caching='unchanged', dtype=np.float32)
         affine = vol.affine
         header = vol.header
 
         mask = nib.load(dataset['mask']).get_fdata(caching='unchanged').astype(np.bool)
-        # std = nib.load(dataset.replace('.nii', '_std.nii.gz')).get_data()
         bvals = np.loadtxt(dataset['bval'])
         bvecs = np.loadtxt(dataset['bvec'])
 
@@ -352,35 +268,15 @@ def harmonize_my_data(dataset, kwargs):
             bvecs = bvecs.T
 
         if center:
-            # # data_mean = np.mean(data, axis=-1, keepdims=True)
-            # with warnings.catch_warnings():
-            #     warnings.filterwarnings('ignore')
-            #     data_mean = np.nanmean(np.where(data != 0, data, np.nan), axis=-1, keepdims=True)
-
-            # # print(np.sum(np.isfinite(data_mean)), np.sum(np.isnan(data_mean)))
-            # data_mean[np.isnan(data_mean)] = 0
-
             # we need to pull down the 3D volume mean for the upsampling part to make sense though
+            # We use nanmean because we implicitly exclude voxels with a value of 0 from the mean this way
             data_mean = np.nanmean(np.where(data != 0, data, np.nan), axis=(0, 1, 2), keepdims=True)
             data -= data_mean
-            # print('centering removed {}'.format(data_mean.mean()))
-            # 1/0
 
-        # if use_std:
-        #     if bias_correct_std:
-        #         print('this is now disabled somehow :/')
-        #         # print(std.shape, data.shape)
-        #         # std = np.broadcast_to(std[..., None], data.shape)
-        #         # mask_4D = np.broadcast_to(mask[..., None], data.shape)
-        #         # std = np.median(corrected_sigma(data, std, mask_4D, N), axis=-1)
-        #     variance = std**2
-        # else:
-        # std = None
         variance = None
 
         # number of blocks is without b0, so we +1 the last dimension
         if len(block_up) < data.ndim:
-            # last_size = int(D.shape[0] / np.prod(block_size))
             current_block_size = block_size[:-1] + (block_size[-1] + 1,)
             current_block_up = block_up + (block_size[-1] + 1,)
         else:
@@ -388,12 +284,9 @@ def harmonize_my_data(dataset, kwargs):
             current_block_up = block_up[:-1] + (block_up[-1] + 1,)
 
         print('Output filename is {}'.format(output_filename))
-        print(D.shape, current_block_size, current_block_up)
 
         factor = np.divide(current_block_up, current_block_size)
 
-        # split_b0s = True
-        # b0_threshold = 20
         b0_loc = np.where(bvals <= b0_threshold)[0]
         dwis = np.where(bvals > b0_threshold)[0]
         num_b0s = len(b0_loc)
@@ -441,11 +334,8 @@ def harmonize_my_data(dataset, kwargs):
         predicted = np.zeros(predicted_size, dtype=np.float32)
         divider = np.zeros(predicted.shape[-1])
 
-        print(data.shape, predicted.shape, factor)
-
         # Put all idx + b0 in this array in each iteration
         to_denoise = np.empty(data.shape[:-1] + (current_block_size[-1],), dtype=np.float64)
-        print(to_denoise.shape)
 
         for i, idx in enumerate(indexes, start=1):
             b0_loc = tuple((next(split_b0s_idx),))
@@ -453,7 +343,7 @@ def harmonize_my_data(dataset, kwargs):
             to_denoise[..., 1:] = data[..., idx]
             divider[list(b0_loc + idx)] += 1
 
-            print('Now denoising volumes {} / block {} out of {}.'.format(b0_loc + idx, i, len(indexes)))
+            print('Now rebuilding volumes {} / block {} out of {}.'.format(b0_loc + idx, i, len(indexes)))
             predicted[..., b0_loc + idx] += rebuild(to_denoise,
                                                     mask,
                                                     D,
@@ -466,47 +356,13 @@ def harmonize_my_data(dataset, kwargs):
                                                     fit_intercept=fit_intercept,
                                                     use_crossval=use_crossval,
                                                     variance=variance)
-            # break
         predicted /= divider
 
         if center:
-            predicted += data_mean
-            # el cheapo mask after upsampling
-            predicted[predicted == data_mean] = 0
+            predicted[mask] += data_mean
 
         # clip negatives, which happens at the borders
         predicted.clip(min=0., out=predicted)
 
-        # header voxel size is all screwed up, so replace with the destination header and affine by the matching dataset
-        # to_affine = nib.load(to_dataset).affine
-        # to_header = nib.load(to_dataset).header
-        # header['pixdim'] = to_header['pixdim']
-
         imgfile = nib.Nifti1Image(predicted, affine, header)
         nib.save(imgfile, output_filename)
-
-        # # subsample to common bvals/bvecs/tr/te
-        # to_data = nib.load(to_dataset).get_data()
-        # to_bvals = np.loadtxt(to_dataset.replace('.nii', '.bval'))
-        # to_bvecs = np.loadtxt(to_dataset.replace('.nii', '.bvec'))
-
-        # to_indexer = get_indexer(to_dataset)
-        # to_data = to_data[..., to_indexer]
-        # to_bvals = to_bvals[to_indexer]
-        # to_bvecs = to_bvecs[to_indexer]
-
-        # np.savetxt(to_dataset.replace('dwi.nii', 'dwi_subsample.bval'), to_bvals, fmt='%1.4f')
-        # np.savetxt(to_dataset.replace('dwi.nii', 'dwi_subsample.bvec'), to_bvecs, fmt='%1.4f')
-
-        # # match to the new bvecs
-        # rotated = match_bvecs(predicted, bvals, bvecs, to_bvals, to_bvecs).clip(min=0.)
-        # imgfile = nib.Nifti1Image(rotated, to_affine, to_header)
-        # nib.save(imgfile, output_filename.replace('predicted', 'rotated'))
-
-        # # match to the new bvecs with nnls
-        # rotated_nnls = match_bvecs(predicted, bvals, bvecs, to_bvals, to_bvecs, use_nnls=True)
-        # imgfile = nib.Nifti1Image(rotated_nnls, to_affine, to_header)
-        # nib.save(imgfile, output_filename.replace('predicted', 'rotated_nnls'))
-
-        # del data, predicted, vol, imgfile, mask, variance
-        # del to_denoise, divider
