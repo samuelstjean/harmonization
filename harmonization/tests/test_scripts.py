bdc86c15f7412fa1328ba88dfdc35db40348c11b
Samuel St-Jean <3030760+samuelstjean@users.noreply.github.com>
2024-11-18 11:30:31 -0500

    revamp tests

--- harmonization/tests/test_scripts.py
+++ harmonization/tests/test_scripts.py
@@ -1,8 +1,12 @@
+import os.path
 import subprocess
 import pytest
 
-commands = ['harmonization_get_global_D config.yaml',
-            'harmonization_harmonize_my_data config.yaml']
+path = os.path.dirname(os.path.realpath(__file__))
+filename = os.path.join(path, 'datasets', 'config.yaml')
+
+commands = [f'harmonization_get_global_D {filename}',
+            f'harmonization_harmonize_my_data {filename}']
 
 @pytest.mark.parametrize('command', commands)
 def test_scripts(command):
