b3d458e0d7d286ec2dc4d536ed8ad2cbdd1243ad
Samuel St-Jean <3030760+samuelstjean@users.noreply.github.com>
2024-12-12 13:30:21 -0500

    testing stuff

--- harmonization/tests/test_scripts.py
+++ harmonization/tests/test_scripts.py
@@ -4,9 +4,14 @@ import pytest
 
 path = os.path.dirname(os.path.realpath(__file__))
 filename = os.path.join(path, 'datasets', 'config.yaml')
+filename_upscale = os.path.join(path, 'datasets', 'config_upscale.yaml')
 
-commands = [f'harmonization_get_global_D {filename}',
-            f'harmonization_harmonize_my_data {filename}']
+files = [filename, filename_upscale]
+commands = []
+
+for fname in files:
+    commands += [f'harmonization_get_global_D {fname}',
+                 f'harmonization_harmonize_my_data {fname}']
 
 @pytest.mark.parametrize('command', commands)
 def test_scripts(command):
